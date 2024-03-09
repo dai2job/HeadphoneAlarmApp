10b99611a52cd7ab40db3abcf48ed9804cf52247
matsubara <dai2dia.job@gmail.com>
2024-03-09 20:49:21 +0900

    Add function to display current time

--- app/src/main/java/com/matsubara/headphonealarmapp/MainActivity.java
+++ app/src/main/java/com/matsubara/headphonealarmapp/MainActivity.java
@@ -3,27 +3,52 @@ package com.matsubara.headphonealarmapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.matsubara.headphonealarmapp.connector.SoundConnector;
+import com.matsubara.headphonealarmapp.connector.TimeConnector;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    Button beepButton = null;
+    private TextView currentTimeTextView;
+    Button beepButton;
+    TimeConnector mTimeConnector = null;
     SoundConnector mSoundConnector = null;
+
+    private final Handler handler = new Handler(Looper.getMainLooper());
+
+    private final Runnable updateCurrentTimeTask = new Runnable() {
+        @Override
+        public void run() {
+            updateCurrentTime();
+            handler.postDelayed(this, 1000); // 1秒ごとに更新
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initializeButton();
+        initializeView();
+        mTimeConnector = new TimeConnector();
         mSoundConnector = new SoundConnector(this, R.raw.beep_sound);
+
+        handler.post(updateCurrentTimeTask);
     }
 
-    public void initializeButton() {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(updateCurrentTimeTask);
+    }
+
+    public void initializeView() {
         beepButton = findViewById(R.id.beep_button);
         beepButton.setOnClickListener(view -> {
             if (mSoundConnector == null) {
@@ -32,5 +57,11 @@ public class MainActivity extends AppCompatActivity {
             }
             mSoundConnector.beep();
         });
+
+        currentTimeTextView = findViewById(R.id.current_time_text);
+    }
+
+    private void updateCurrentTime() {
+        currentTimeTextView.setText(mTimeConnector.getCurrentTime());
     }
 }
